1e75cfe8235aff780b528a98bd4511a964ff6160
Joshua Shoemaker <joshuashoemaker@outlook.com>
2018-10-08 13:57:58 -0500

    feat: replaced PUT with PATCH method

--- index.js
+++ index.js
@@ -1,72 +1,64 @@
-const express = require('express');
-const app = express();
-    
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
 
-const axios = require('axios');
+const bodyParser = require('body-parser')
 
+const axios = require('axios')
 
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin || "*");
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type,x-requested-with');
-    next();
-  });
-
-
-app.post('/interface', (req, res) => {
-
-
-    const data = req.body.data;
-
-    if(data.method === "POST"){
-        post(data)
-        .then(function(response){
-            res.send(response.data);
-        })
-        .catch(function (error) {
-          res.send(error.response.data);
-        });
-    }
-    else if(data.method === "GET"){
-        get(data)
-        .then(function(response){
-            res.send(response.data);
-        })
-        .catch(function(error){
-            res.send(error.response.data);
-        })
-    }
-    else if(data.method === "PUT"){
-        put(data)
-        .then(function(response){
-            res.send(response.data);
-        })
-        .catch(function(error){
-            res.send(error.response.data);
-        })
-    }
-    else{
-        res.send('Request not possible. Most likly this error is cause by the "Method" not being set in your request body');
-    }
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*')
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS,PATCH')
+  res.header('Access-Control-Allow-Headers', 'Content-Type,x-requested-with')
+  next()
 })
 
+app.post('/interface', (req, res) => {
+  const data = req.body.data
 
-const post = data =>{
-    return axios.post(data.url, data.body, {headers : data.headers});
+  if (data.method === 'POST') {
+    post(data)
+      .then(function (response) {
+        res.send(response.data)
+      })
+      .catch(function (error) {
+        res.send(error.response.data)
+      })
+  } else if (data.method === 'GET') {
+    get(data)
+      .then(function (response) {
+        res.send(response.data)
+      })
+      .catch(function (error) {
+        res.send(error.response.data)
+      })
+  } else if (data.method === 'PATCH') {
+    patch(data)
+      .then(function (response) {
+        res.send(response.data)
+      })
+      .catch(function (error) {
+        res.send(error.response.data)
+      })
+  } else {
+    res.send('Request not possible. Most likly this error is cause by the "Method" not being set in your request body')
+  }
+})
+
+const post = data => {
+  return axios.post(data.url, data.body, {headers: data.headers})
 }
 
-const get = data =>{
-    return axios.get(data.url,  {headers : data.headers});
+const get = data => {
+  return axios.get(data.url, {headers: data.headers})
 }
 
-const put = data =>{
-    return axios.put(data.url, data.body,  {headers : data.headers});
+const patch = data => {
+  return axios.put(data.url, data.body, {headers: data.headers})
 }
 
 const port = process.env.PORT || 3000
 
-app.listen(port, () => console.log("FM Data API Interface listening on port " + port));
+app.listen(port, () => console.log('FM Data API Interface listening on port ' + port))
